84e4371382e1ebe42398bf523d47a7b2aff4c944
Hugo Flores <hfp.ing2@gmail.com>
2020-03-26 12:09:36 -0400

    Demo App and Base Project

---
+++ TheSimpsonsApp/TheSimpsonsApp/Controllers/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TheSimpsonsApp
+//
+//  Created by Field Employee on 3/26/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
