dd10cf6ec0e00ccfaa9057f4c580d92d34ee8a5a
Hugo Flores <hfp.ing2@gmail.com>
2020-03-27 15:48:43 -0400

    Name and description separation

--- TheSimpsonsApp/TheSimpsonsApp/Models/SimpsonResponse.swift
+++ TheSimpsonsApp/TheSimpsonsApp/Models/SimpsonResponse.swift
@@ -21,6 +21,12 @@ struct RelatedTopic: Decodable {
         case swift, combine, debugging, xcode
     }
 
+    var charName: String {
+        return self.Text.components(separatedBy: " - ")[0]
+    }
+    var charDescription: String {
+        return self.Text.components(separatedBy: " - ")[1]
+    }
     let Text: String
     let Icon: Icon
 }
@@ -47,5 +53,14 @@ class SimpsonResponse {
             }
         }
         return nil
+    }
+    
+    func createDummyData() -> MainResponse {
+        let data = [
+            RelatedTopic(Text: "Apu Nahasapeemapetilon - Apu Nahasapeemapetilon is a recurring character in the animated TV series The Simpsons. He is an Indian-American immigrant proprietor who runs the Kwik-E-Mart, a popular convenience store in Springfield, and is best known for his catchphrase, \"Thank you, come again.\"", Icon: Icon(URL: "https://duckduckgo.com/i/99b04638.png")),
+            RelatedTopic(Text: "Barney Gumble - Barnard Arnold \"Barney\" Gumble is a recurring character in the American animated TV series The Simpsons. He is voiced by Dan Castellaneta and first appeared in the series premiere episode \"Simpsons Roasting on an Open Fire\". Barney is the town drunk of Springfield and Homer Simpson's best friend.", Icon: Icon(URL: "")),
+            RelatedTopic(Text: "Bart Simpson - Bartholomew JoJo Simpson is a fictional character in the American animated television series The Simpsons and part of the Simpson family. He is voiced by Nancy Cartwright and first appeared on television in The Tracey Ullman Show short \"Good Night\" on April 19, 1987.", Icon: Icon(URL: "https://duckduckgo.com/i/39ce98c0.png"))]
+        let res = MainResponse(Heading: "Some Header", RelatedTopics: data);
+        return res;
     }
 }
