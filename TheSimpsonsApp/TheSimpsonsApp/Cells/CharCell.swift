f9b4df20bf32057e56b7d6db42a85e67d957d754
Hugo Flores <hfp.ing2@gmail.com>
2020-03-27 08:59:10 -0400

    Character master table cell

---
+++ TheSimpsonsApp/TheSimpsonsApp/Cells/CharCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharCell.swift
+//  TheSimpsonsApp
+//
+//  Created by Field Employee on 3/27/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+import UIKit
+
+class CharCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
