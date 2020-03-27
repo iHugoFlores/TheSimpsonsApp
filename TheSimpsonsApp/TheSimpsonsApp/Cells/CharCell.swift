57975f947d3c38510b3bd51d641c4c8dc21dd142
Hugo Flores <hfp.ing2@gmail.com>
2020-03-27 15:48:25 -0400

    Custom cell created

--- TheSimpsonsApp/TheSimpsonsApp/Cells/CharCell.swift
+++ TheSimpsonsApp/TheSimpsonsApp/Cells/CharCell.swift
@@ -9,16 +9,81 @@
 import UIKit
 
 class CharCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    var charInfo: RelatedTopic? {
+        didSet {
+            nameLabel.text = charInfo?.charName
+            if (charInfo?.Icon.URL.isEmpty)! {
+                charImage.backgroundColor = .clear
+                charImage.image = UIImage(imageLiteralResourceName: "user")
+            } else {
+                charImage.backgroundColor = .blue
+                charImage.image = nil
+            }
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private let nameLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .black
+        lbl.font = UIFont.boldSystemFont(ofSize: 18)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+    
+    private let charImage : UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFit
+        return imgView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUICell()
     }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setUICell() {
+        setCharImage()
+        setNameLabel()
+    }
+    
+    func setCharImage() {
+        charImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(charImage)
+        let margins = self.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            charImage.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            charImage.topAnchor.constraint(equalTo: margins.topAnchor),
+            charImage.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            
+            charImage.widthAnchor.constraint(equalToConstant: 100),
+            charImage.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    func setNameLabel() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
 
+        addSubview(nameLabel)
+        
+        let margins = charImage.layoutMargins
+        NSLayoutConstraint.activate([
+            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: charImage.trailingAnchor, constant: margins.right),
+            nameLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor)
+            /*
+            nameLabel.topAnchor.constraint(equalTo: margins.topAnchor),
+             nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 18),
+             nameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 18),
+             nameLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 18)
+            */
+        ])
+    }
+    
 }
