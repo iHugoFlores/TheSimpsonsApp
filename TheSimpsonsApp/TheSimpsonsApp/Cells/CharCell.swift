0de9fdd3854e080424091ac15b235c2f3c19cb2b
Hugo Flores <hfp.ing2@gmail.com>
2020-03-28 11:58:03 -0400

    Image download handled

--- TheSimpsonsApp/TheSimpsonsApp/Cells/CharCell.swift
+++ TheSimpsonsApp/TheSimpsonsApp/Cells/CharCell.swift
@@ -14,11 +14,9 @@ class CharCell: UITableViewCell {
         didSet {
             nameLabel.text = charInfo?.charName
             if (charInfo?.Icon.URL.isEmpty)! {
-                charImage.backgroundColor = .clear
-                charImage.image = UIImage(imageLiteralResourceName: "user")
+                setPlaceholderImage()
             } else {
-                charImage.backgroundColor = .blue
-                charImage.image = nil
+                downloadImage()
             }
         }
     }
@@ -37,6 +35,8 @@ class CharCell: UITableViewCell {
         imgView.contentMode = .scaleAspectFit
         return imgView
     }()
+    
+    var vSpinner : UIView?
     
     /*
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -85,6 +85,54 @@ class CharCell: UITableViewCell {
             nameLabel.leadingAnchor.constraint(equalTo: charImage.trailingAnchor, constant: margins.right),
             nameLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor)
         ])
+    }
+    
+    func setPlaceholderImage() {
+        charImage.backgroundColor = .clear
+        charImage.image = UIImage(imageLiteralResourceName: "user")
+    }
+    
+    func downloadImage() {
+        setImageSpinner()
+        SimpsonResponse.downloadImage(fromURL: URL(string: (charInfo?.Icon.URL)!)!, onDone: self.setDownloadedImage)
+    }
+    
+    func setDownloadedImage(image img: UIImage?) {
+        removeSpinner()
+        if (img?.size.height)! <= 1.0 {
+            setPlaceholderImage()
+            return
+        }
+        charImage.image = img
+    }
+    
+    func setImageSpinner() {
+        charImage.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)
+        charImage.image = nil
+        showSpinner(onView: charImage)
+    }
+    
+    func showSpinner(onView : UIView) {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        let ai = UIActivityIndicatorView.init(style: UIActivityIndicatorView.Style.large)
+        ai.startAnimating()
+        
+        spinnerView.addSubview(ai)
+        onView.addSubview(spinnerView)
+        
+        ai.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            ai.centerXAnchor.constraint(equalTo: onView.centerXAnchor),
+            ai.centerYAnchor.constraint(equalTo: onView.centerYAnchor),
+        ])
+        
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner() {
+        self.vSpinner?.removeFromSuperview()
+        self.vSpinner = nil
     }
     
 }
